0a0c7b63801a4884e8efd78c959664ea3703d8ed
Hrishi Bakshi <hrishi@amazon.com>
2017-11-12 21:07:12 -0500

    Fix setup.py trove classifiers

--- setup.py
+++ setup.py
@@ -48,10 +48,10 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache License 2.0',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
